0ab4677edebfe654d4706c22b10fe8178aa488f1
shRahat10 <shahriar.haque.1011@gmail.com>
2024-04-26 19:39:12 +0600

    design, toast and sweet alert

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,8 +3,6 @@ import { useContext, useState } from "react";
 import { IoEyeOffOutline, IoEyeOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { AuthContext } from "./provider/AuthProvider";
-import { toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 
