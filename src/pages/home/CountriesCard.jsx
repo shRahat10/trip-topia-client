d24bf1c2152aeb3315516e6b6722d4be92534159
shRahat10 <shahriar.haque.1011@gmail.com>
2024-04-29 10:59:00 +0600

    dark mode

--- src/pages/home/CountriesCard.jsx
+++ src/pages/home/CountriesCard.jsx
@@ -22,7 +22,7 @@ const CountriesCard = () => {
                 {
                     dataCountry?.map((e, idx) => (
                         <Link to={`/country-spots/${e.country}`} key={idx}>
-                            <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+                            <div className=" shadow-lg rounded-lg overflow-hidden">
                                 <img className="h-56 w-full object-cover object-center rounded-t-lg" src={e.image} alt="Country" />
                                 <div className="p-4">
                                     <p className="text-xl font-semibold mb-2 text-primary">{e.country}</p>
--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../provider/AuthProvider";
 import userIcon from "../../assets/images/userIcon.webp"
@@ -6,6 +6,41 @@ import userIcon from "../../assets/images/userIcon.webp"
 const Navbar = () => {
     const { user, userLogout, loading } = useContext(AuthContext);
     const [isOpen, setIsOpen] = useState(false);
+
+
+    const [theme, setTheme] = useState("light");
+    const html = document.documentElement;
+
+    useEffect(() => {
+        const theme = localStorage.getItem("theme");
+
+        if (theme) {
+            setTheme(theme);
+            html.classList.remove('light');
+            html.classList.add(theme);
+        }
+    }, []);
+
+    // useEffect(() => {
+    //     localStorage.setItem("theme", theme);
+    // }, [theme])
+
+
+    const handleTheme = () => {
+        // console.log("clicked");
+
+        if (theme === "light") {
+            html.classList.remove("light");
+            html.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+            setTheme("dark");
+        } else {
+            html.classList.remove("dark");
+            html.classList.add("light");
+            localStorage.setItem("theme", "light");
+            setTheme("light");
+        }
+    };
 
     const handleUserLogout = () => {
         userLogout();
@@ -83,6 +118,11 @@ const Navbar = () => {
             </div>
 
             <div className="navbar-end">
+                <label onClick={handleTheme} className="cursor-pointer grid place-items-center mr-3">
+                    <input type="checkbox" checked={theme === 'dark'} value="synthwave" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
+                    <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
+                    <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                </label>
                 {
                     loading ? (
                         <div className=" flex justify-end">
@@ -90,12 +130,12 @@ const Navbar = () => {
                         </div>
                     ) : user ? (
                         <div className=" flex gap-2 items-center">
-                            <div className="tooltip tooltip-left" data-tip={user.displayName} >
-                                <img className=" w-12 h-12 rounded-full border border-primary object-cover object-center" src={user.photoURL ? user.photoURL : userIcon} alt="" />
-                            </div>
                             <button onClick={handleUserLogout} className="bg-primary text-white py-2 px-4 rounded hover:bg-transparent hover:outline hover:outline-1 hover:outline-primary hover:text-primary transition duration-300 ease-in-out">
                                 Logout
                             </button>
+                            <div className="tooltip tooltip-left" data-tip={user.displayName} >
+                                <img className=" w-12 h-12 rounded-full border border-primary object-cover object-center" src={user.photoURL ? user.photoURL : userIcon} alt="" />
+                            </div>
                         </div>
 
                     ) : (
