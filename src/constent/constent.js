bb0dd08941aad7611713c6e913190adde59c7f16
shRahat10 <shahriar.haque.1011@gmail.com>
2024-04-29 09:37:35 +0600

    deployment

--- src/constent/constent.js
+++ src/constent/constent.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:5000'
-// export const BASE_URL = 'https://trip-topia-server-dj5vzbms4-shrahats-projects.vercel.app'
+// export const BASE_URL = 'http://localhost:5000'
+export const BASE_URL = 'https://trip-topia-server-shrahats-projects.vercel.app'
