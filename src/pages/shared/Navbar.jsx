da4147cac7126fc784d28371ad8ebe3c32c16248
shRahat10 <shahriar.haque.1011@gmail.com>
2024-04-30 17:29:45 +0600

    final

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -4,6 +4,8 @@ import { AuthContext } from "../provider/AuthProvider";
 import userIcon from "../../assets/images/userIcon.webp";
 import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { FiSun } from "react-icons/fi";
 
 const Navbar = () => {
     const { user, userLogout, loading } = useContext(AuthContext);
@@ -38,10 +40,10 @@ const Navbar = () => {
 
     const handleUserLogout = () => {
         userLogout()
-        .then(()=>{
-            toast.success("User Logged Out")
-        })
-        
+            .then(() => {
+                toast.success("User Logged Out")
+            })
+
     };
 
     useEffect(() => {
@@ -130,11 +132,18 @@ const Navbar = () => {
             </div>
 
             <div className="navbar-end">
-                <label onClick={handleTheme} className="cursor-pointer grid place-items-center mr-3">
+                {/* <label onClick={handleTheme} className="cursor-pointer grid place-items-center mr-3">
                     <input type="checkbox" checked={theme === 'dark'} value="synthwave" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
                     <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
                     <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-                </label>
+                </label> */}
+                <li className="px-6 hidden lg:block ">
+                    {theme === "dark" ? (
+                        <FiSun className="text-xl sm:text-3xl md:text-3xl text-zinc-700" onClick={handleTheme} />
+                    ) : (
+                        <BsFillMoonStarsFill className="text-lg sm:text-xl md:text-2xl" onClick={handleTheme} />
+                    )}
+                </li>
                 {
                     loading ? (
                         <div className=" flex justify-end">
